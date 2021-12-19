4ae4521d213e91b38269cc9b0ebcb0ede62adfe5
rlss91 <rlss91@gmail.com>
2021-12-19 12:51:02 +0200

    ajax post

--- firstAccess/js/ajax.js
+++ firstAccess/js/ajax.js
@@ -2,7 +2,16 @@ window.addEventListener("load", () => {
   sendXHRCallBack(
     "https://jsonplaceholder.typicode.com/posts",
     function (res) {
-      console.log("response from server using post request", res);
+      let resObj = JSON.parse(res);
+      document.getElementById("postsDiv").innerHTML += `
+    <div class="card" style="width: 18rem;" id="post_${resObj.id}">
+      <div class="card-body">
+        <h5 class="card-title">${resObj.title}</h5>
+        <p class="card-text">${resObj.body}</p>
+        <button type="button" class="btn btn-danger" onclick="handleDeleteBtnClick('post_${resObj.id}', '${resObj.id}')">Delete</button>
+      </div>
+    </div>
+    `;
     },
     "POST",
     {
@@ -60,52 +69,52 @@ function handleDeleteBtnClick(elmid, serverid) {
   );
 }
 
-function sendXHRGET(url) {
-  //create ajax request
-  const xhttp = new XMLHttpRequest();
-  //capture ready state
-  xhttp.onreadystatechange = function () {
-    // console.log("this.readyState", this.readyState);
-    // console.log("this.status", this.status);
-    // console.log("this.responseText", this.responseText);
-    //when the response was successful then it will display the response in the console
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-      console.log(this.responseText);
-    }
-  };
-  xhttp.onerror = function (errorEvent) {
-    console.log("ERRO", errorEvent);
-  };
-  //set the method and the url of the ajax request
-  xhttp.open("GET", url, true);
-  //send the actual request
-  xhttp.send();
-}
+// function sendXHRGET(url) {
+//   //create ajax request
+//   const xhttp = new XMLHttpRequest();
+//   //capture ready state
+//   xhttp.onreadystatechange = function () {
+//     // console.log("this.readyState", this.readyState);
+//     // console.log("this.status", this.status);
+//     // console.log("this.responseText", this.responseText);
+//     //when the response was successful then it will display the response in the console
+//     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+//       console.log(this.responseText);
+//     }
+//   };
+//   xhttp.onerror = function (errorEvent) {
+//     console.log("ERRO", errorEvent);
+//   };
+//   //set the method and the url of the ajax request
+//   xhttp.open("GET", url, true);
+//   //send the actual request
+//   xhttp.send();
+// }
 
-//this function ask for url and callback function
-function sendXHRGETCallBack(url, cb) {
-  //create ajax request
-  const xhttp = new XMLHttpRequest();
-  //capture ready state
-  xhttp.onreadystatechange = function () {
-    // console.log("this.readyState", this.readyState);
-    // console.log("this.status", this.status);
-    // console.log("this.responseText", this.responseText);
-    //when the response was successful then it will display the response in the console
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-      // console.log(this.responseText);
-      //call for the callback function
-      cb(this.responseText);
-    }
-  };
-  xhttp.onerror = function (errorEvent) {
-    console.log("ERRO", errorEvent);
-  };
-  //set the method and the url of the ajax request
-  xhttp.open("GET", url, true);
-  //send the actual request
-  xhttp.send();
-}
+// //this function ask for url and callback function
+// function sendXHRGETCallBack(url, cb) {
+//   //create ajax request
+//   const xhttp = new XMLHttpRequest();
+//   //capture ready state
+//   xhttp.onreadystatechange = function () {
+//     // console.log("this.readyState", this.readyState);
+//     // console.log("this.status", this.status);
+//     // console.log("this.responseText", this.responseText);
+//     //when the response was successful then it will display the response in the console
+//     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+//       // console.log(this.responseText);
+//       //call for the callback function
+//       cb(this.responseText);
+//     }
+//   };
+//   xhttp.onerror = function (errorEvent) {
+//     console.log("ERRO", errorEvent);
+//   };
+//   //set the method and the url of the ajax request
+//   xhttp.open("GET", url, true);
+//   //send the actual request
+//   xhttp.send();
+// }
 
 //this function ask for url and callback function
 function sendXHRCallBack(url, cb, method = "GET", body = undefined) {
@@ -113,15 +122,11 @@ function sendXHRCallBack(url, cb, method = "GET", body = undefined) {
   const xhttp = new XMLHttpRequest();
   //capture ready state
   xhttp.onreadystatechange = function () {
-    // console.log("this.readyState", this.readyState);
-    // console.log("this.status", this.status);
-    // console.log("this.responseText", this.responseText);
     //when the response was successful then it will display the response in the console
     if (
       this.readyState == XMLHttpRequest.DONE &&
       (this.status == 200 || this.status == 201)
     ) {
-      // console.log(this.responseText);
       //call for the callback function
       cb(this.responseText);
     }
