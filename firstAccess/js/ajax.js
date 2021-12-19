dd50f4267b82d0317a320d0452aceedfdb1bf06f
rlss91 <rlss91@gmail.com>
2021-12-19 11:23:10 +0200

    ajax cb

--- firstAccess/js/ajax.js
+++ firstAccess/js/ajax.js
@@ -2,6 +2,7 @@ window.addEventListener("load", () => {});
 
 function handleBtnClick() {
   let num = document.getElementById("idtxt").value;
+  //"https://jsonplaceholder.typicode.com/posts/1
   sendXHRGET("https://jsonplaceholder.typicode.com/posts/" + num);
 }
 
@@ -18,8 +19,32 @@ function sendXHRGET(url) {
       console.log(this.responseText);
     }
   };
-  xhttp.onerror = function (ev) {
-    console.log("ERRO", ev);
+  xhttp.onerror = function (errorEvent) {
+    console.log("ERRO", errorEvent);
+  };
+  //set the method and the url of the ajax request
+  xhttp.open("GET", url, true);
+  //send the actual request
+  xhttp.send();
+}
+
+//this function ask for url and callback function
+function sendXHRGETCallBack(url, cb) {
+  //create ajax request
+  const xhttp = new XMLHttpRequest();
+  //capture ready state
+  xhttp.onreadystatechange = function () {
+    // console.log("this.readyState", this.readyState);
+    // console.log("this.status", this.status);
+    // console.log("this.responseText", this.responseText);
+    //when the response was successful then it will display the response in the console
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+      // console.log(this.responseText);
+      cb(this.responseText);
+    }
+  };
+  xhttp.onerror = function (errorEvent) {
+    console.log("ERRO", errorEvent);
   };
   //set the method and the url of the ajax request
   xhttp.open("GET", url, true);
